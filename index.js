e9a0489c73b46b22200e082069046fe4a7e6ff08
Prisha <pkandhari_be22@thapar.edu>
2024-06-25 14:12:01 +0530

    added css

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(express.static(path.join(__dirname,"public")))  //this will grant access to the public folder directly
+//any request that tries to access a file will be directed to the public folder
+
 app.use("/admin", adminRoutes); //not as a function but as an object , it will automatically call the router object and execute the routes and consider the routes as a middleware
 app.use(shopRoutes); //not as a function but as an object , it will automatically call the router object and execute the routes and consider the routes as a middleware
 
